1acfcaacf00d9d06209c20b02c876604fcae6148
Niklas Pieper <pieper.niklas.gm@gmail.com>
2025-04-04 09:58:30 +0200

    Cleanup testclass and add a new test

--- src/test/java/com/example/spring_boot/service/AuthenticationServiceTest.java
+++ src/test/java/com/example/spring_boot/service/AuthenticationServiceTest.java
@@ -1,18 +1,22 @@
 package com.example.spring_boot.service;
 
+import com.example.spring_boot.DTO.LoginUserDto;
 import com.example.spring_boot.DTO.RegisterUserDto;
 import com.example.spring_boot.entity.User;
 import com.example.spring_boot.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -25,13 +29,18 @@ public class AuthenticationServiceTest {
     @Mock
     private PasswordEncoder passwordEncoder;
 
+    @Mock
+    private  AuthenticationManager authenticationManager;
+
     @InjectMocks
     private AuthenticationService authenticationService; // Testet nur die Logik dieser Klasse
 
     private static final String EMAIL = "abc@gmail.com";
+    private static final String PASSWORD = "123";
+    private static final String FULL_NAME = "Max Mustermann";
 
     @Test
-    public void testSignup() {
+    public void signup_ShouldSaveUserWithEncodedPassword() {
         User user = User.builder().id(1)
                 .email("abc@gmail.com")
                 .fullName("Test User")
@@ -40,16 +49,13 @@ public class AuthenticationServiceTest {
 
         RegisterUserDto registerUserDto = RegisterUserDto.builder()
                 .email(EMAIL)
-                .password("12")
-                .fullName("Hallo")
+                .password(PASSWORD)
+                .fullName(FULL_NAME)
                 .build();
 
-
-       // when(userRepository.save(any(User.class))).thenReturn(user);
         when(userRepository.save(any(User.class))).thenAnswer(invocation -> {
             return invocation.getArgument(0); // Der User, der tatsächlich gespeichert wird
         });
-
 
         // Act
         User savedUser = authenticationService.signup(registerUserDto);
@@ -58,29 +64,40 @@ public class AuthenticationServiceTest {
         assertEquals(EMAIL, savedUser.getUsername());
         verify(userRepository, times(1)).save(savedUser);
         verify(passwordEncoder, times(1)).encode(registerUserDto.getPassword());
-
     }
-/*
+
     @Test
-    public void testLogin_WithValidUser() {
-        // Arrange
-        User user = new User("testuser", "password");
-        when(userRepository.findByUsername("testuser")).thenReturn(Optional.of(user));
+    public void authenticate_ShouldFindUserByEmail() {
+        LoginUserDto loginUser = LoginUserDto.builder()
+                .email(EMAIL)
+                .password(PASSWORD)
+                .build();
+        User user = User.builder().id(1)
+                .email(EMAIL)
+                .fullName(FULL_NAME)
+                .password(PASSWORD)
+                .build();
+
+
+        when(userRepository.findByEmail(EMAIL)).thenReturn(Optional.of(user));
+
+        ArgumentCaptor<UsernamePasswordAuthenticationToken> captor =
+                ArgumentCaptor.forClass(UsernamePasswordAuthenticationToken.class);
 
         // Act
-        User foundUser = authService.login("testuser", "password");
+        User savedUser = authenticationService.authenticate(loginUser);
 
-        // Assert
-        assertNotNull(foundUser);
-        assertEquals("testuser", foundUser.getUsername());
+        // Verify that UsernamePasswordAuthenticationToken gets called with right parameter
+        verify(authenticationManager).authenticate(captor.capture());
+        UsernamePasswordAuthenticationToken capturedToken = captor.getValue();
+        assertEquals(EMAIL, capturedToken.getPrincipal());
+        assertEquals(PASSWORD, capturedToken.getCredentials());
+
+        assertTrue(passwordEncoder.matches(PASSWORD, savedUser.getPassword()));
+
+
+        verify(userRepository, times(1)).findByEmail(loginUser.getEmail());
+        assertEquals(FULL_NAME, savedUser.getFullName());
+        assertEquals(EMAIL, savedUser.getUsername());
     }
-
-    @Test
-    public void testLogin_WithInvalidUser() {
-        // Arrange
-        when(userRepository.findByUsername("wronguser")).thenReturn(Optional.empty());
-
-        // Act & Assert
-        assertThrows(RuntimeException.class, () -> authService.login("wronguser", "password"));
-    } */
 }
